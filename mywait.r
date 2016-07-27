8577f432ea43b4cbdb533614d6da2a251a95b806
clairecDS <chuc@mail.smu.edu>
2016-07-27 12:20:03 -0700

    Delete mywait.r

--- mywait.r
+++
@@ -1,6 +0,0 @@
-mywait <- function() {
-  tt <- tktoplevel()
-  tkpack( tkbutton(tt, text='Continue', command=function()tkdestroy(tt)), side='bottom')
-    tkbind(tt,'<Key>', function()tkdestroy(tt) )
-    tkwait.window(tt)
-}
